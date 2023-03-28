3c1aac7e7f14f7c19e67e122d6514e6deed23c25
GameDog9988 <alex@grattan.me>
2023-03-27 22:45:00 -0400

    feat: fetch location data for slug page

--- routes/[slug].tsx
+++ routes/[slug].tsx
@@ -1,5 +1,26 @@
-import { PageProps } from "$fresh/server.ts";
+import Header from "../islands/Header.tsx";
+import { Handlers, PageProps } from "$fresh/server.ts";
+import { Kitchen } from "../types/kitchen.ts";
+import { load } from "std/dotenv/mod.ts";
 
-export default function Kitchen(props: PageProps) {
-  return <div>Hello {props.params.slug}</div>;
+export const handler: Handlers<Kitchen | null> = {
+  async GET(_, ctx) {
+    const env = await load();
+    const resp = await fetch(
+      `${env["API_URL"] ?? Deno.env.get("API_URL")}/kitchen/${ctx.params.slug}`
+    );
+    if (resp.status === 404) {
+      return ctx.render(null);
+    }
+    const kitchen: Kitchen = await resp.json();
+    return ctx.render(kitchen);
+  },
+};
+
+export default function Kitchen({ data }: PageProps<Kitchen[] | null>) {
+  return (
+    <div>
+      <pre>{JSON.stringify(data, null, 4)}</pre>
+    </div>
+  );
 }
