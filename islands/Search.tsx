711173a57eef415e0cdb12b83353d818279d3dbb
GameDog9988 <alex@grattan.me>
2023-03-27 22:09:07 -0400

    fix: adjust search stacking for iOS

--- islands/Search.tsx
+++ islands/Search.tsx
@@ -25,7 +25,7 @@ export default function Search({ kitchens }: SearchProps) {
 
   return (
     <form class="group relative w-full max-w-xl z-10">
-      <div class="flex items-center border(black 2) rounded-lg px-6 bg-white">
+      <div class="relative z-10 flex items-center border(black 2) rounded-lg px-6 bg-white">
         <IconSearch class="relative w-6 h-6" />
         <input
           class="relative w-full px-4 py-3 md:text-2xl outline-none"
@@ -35,21 +35,21 @@ export default function Search({ kitchens }: SearchProps) {
           onInput={handleOnInput}
           disabled={!IS_BROWSER}
         />
-        {input !== "" && searchResults.length > 0 && (
-          <ul class="absolute -z-10 origin-bottom pt-8 left-0 right-0 top-8 max-h-64 overflow-hidden w-full bg-white rounded-lg border(black 2)">
-            {searchResults.map((result, i) => (
-              <li key={i}>
-                <a
-                  href={`/${result.slug}`}
-                  class="hover:bg-gray-100 text-sm md:text-base block px-6 py-4"
-                >
-                  {result.name}
-                </a>
-              </li>
-            ))}
-          </ul>
-        )}
       </div>
+      {input !== "" && searchResults.length > 0 && (
+        <ul class="absolute pt-8 left-0 right-0 top-8 max-h-64 overflow-hidden w-full bg-white rounded-lg border(black 2)">
+          {searchResults.map((result, i) => (
+            <li key={i}>
+              <a
+                href={`/${result.slug}`}
+                class="hover:bg-gray-100 text-sm md:text-base block px-6 py-4"
+              >
+                {result.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      )}
     </form>
   );
 }
