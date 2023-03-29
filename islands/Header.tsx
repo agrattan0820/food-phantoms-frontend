be8d91ad622f2f65dc44331bfdf5f31206d59876
GameDog9988 <alex@grattan.me>
2023-03-28 22:33:14 -0400

    feat: header title links to homepage

--- islands/Header.tsx
+++ islands/Header.tsx
@@ -1,11 +1,12 @@
-import { useState } from "preact/hooks";
 import { Button } from "../components/Button.tsx";
 import IconPlus from "tabler/plus.tsx";
 
 export default function Header() {
   return (
     <div class="flex justify-between items-center w-full max-w-screen-lg p-4 mx-auto">
-      <h1 class="font-serif py-1 text-xl md:text-3xl">Food Phantoms</h1>
+      <h1 class="font-serif py-1 text-xl md:text-3xl">
+        <a href="/">Food Phantoms</a>
+      </h1>
       <Button>
         Add a Location <IconPlus />
       </Button>
