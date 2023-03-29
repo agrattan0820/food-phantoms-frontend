9ef91d028109e46b720b8403abfb33d57a66f903
GameDog9988 <alex@grattan.me>
2023-03-28 20:24:54 -0400

    fix: add back in `.env` file loading for kitchen detail page

--- routes/[slug].tsx
+++ routes/[slug].tsx
@@ -1,10 +1,12 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
 import { Kitchen } from "../types/kitchen.ts";
+import { load } from "std/dotenv/mod.ts";
 
 export const handler: Handlers<Kitchen | null> = {
   async GET(_, ctx) {
+    const env = await load();
     const resp = await fetch(
-      `${Deno.env.get("API_URL")}/kitchen/${ctx.params.slug}`
+      `${env["API_URL"] ?? Deno.env.get("API_URL")}/kitchen/${ctx.params.slug}`
     );
     if (resp.status === 404) {
       return ctx.render(null);
