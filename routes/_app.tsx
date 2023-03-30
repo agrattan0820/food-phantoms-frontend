9b9313145648cf87332b3bde93c07bdfece5d895
GameDog9988 <alex@grattan.me>
2023-03-29 22:28:14 -0400

    feat: add ghost favicon

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -7,6 +7,7 @@ export default function App({ Component }: AppProps) {
       <Head>
         <title>Food Phantoms</title>
         <link rel="stylesheet" href={asset("/global.css")} />
+        <link rel="shortcut icon" href={asset("/ghost_favicon.png")} />
       </Head>
       <div class="font-sans">
         <Component />
